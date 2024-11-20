b1ab5cc1d63e9d12c9cfd1d60e4ab73bcdd9ac20
Ludmast <illud263@gmail.com>
2024-11-21 00:24:43 +0300

    Update GlavScript.js

--- GlavScript.js
+++ GlavScript.js
@@ -1,27 +1,28 @@
+//=================================================================================================
 function Sait_load() {
 
-    bob.style.backgroundColor = "#102040";
+    bob.style.backgroundColor = "#306090";
     t = 50;
-
+    text_size = 30;
 
     for (i = 0; i < ogl; i++) {
         odin = document.getElementById("p_" + i);
         if (ogl_vibrono == i) {
             odin.style.top = t - 3 + "px";
-            odin.style.backgroundColor = "#1070a0"
-            odin.style.color = "#a0b0f0"
+            odin.style.backgroundColor = "#1070a0";
+            odin.style.color = "#a0b0f0";
             odin.style.left = "20px";
             odin.style.width = "240px";
-            odin.style.fontSize = "22px";
+            odin.style.fontSize = "30px";
             odin.style.padding = "7px";
         } else {
             odin.style.top = t + "px";
             odin.style.left = "25px";
         }
-        t += 45
+        t += 45;
     }  
     //---------------------------------------------------------------------------------------------
-    in_load.innerHTML = "<div id='name_sait1'></div><div id='name_sait2'></div><table id='menu_line'><tr><td id = 'm_g' class='c_m_g' onmouseover = 'menu_(1,0,0),mn2()' onmouseout = 'menu_(2,0,0)' ></td ><td>|</td><td id='m_y' class='c_m_g' onmouseover='menu_(1,0,1),mn2()' onmouseout='menu_(2,0,1)'></td><td>|</td><td id='m_os' class='c_m_g' onmouseover='menu_(1,0,2),mn2()' onmouseout='menu_(2,0,2)'></td><td>|</td><td id='m_s' class='c_m_g' onmouseover='menu_(1,0,3),mn2()' onmouseout='menu_(2,0,3)'></td></tr><tr><td id='m1'colspan='2'></td><td id='m2'colspan='2'></td><td id='m3'colspan='2'></td><td id='m4'></td></tr></table><hr id='line_pomoshnik' color='black' />"
+    in_load.innerHTML = "<div id='name_sait1'></div><div id='name_sait2'></div><div id='menu_line'></div><table id='menu'><tr><td id = 'm_g' class='c_m_g' onmouseover = 'menu_(1,0,0)' onmouseout = 'menu_(2,0,0)' ></td ><td>|</td><td id='m_y' class='c_m_g' onmouseover='menu_(1,0,1)' onmouseout='menu_(2,0,1)'></td><td>|</td><td id='m_os' class='c_m_g' onmouseover='menu_(1,0,2)' onmouseout='menu_(2,0,2)'></td><td>|</td><td id='m_s' class='c_m_g' onmouseover='menu_(1,0,3)' onmouseout='menu_(2,0,3)'></td></tr><tr><td id='m1'></td><td id='m2'></td><td id='m3'></td><td id='m4'></td></tr></table><hr id='line_pomoshnik' color='#80b0ff' />";
     //---------------------------------------------------------------------------------------------
     name_sait1.innerText = array_name_sait[0];
    // name_sait1_teni.innerText = array_name_sait[0];
@@ -29,9 +30,10 @@ function Sait_load() {
    // name_sait2_teni.innerText = array_name_sait[1];
     ns_a = 2; ns_b = 1; ns_i = 0;
     ns_c = ns_d = 0;
+    //---------------------------------------------------------------------------------------------
 
     m1_left = 5; m2_left = 89; m3_left = 189; m4_left = 313; m_b = 1;
-    ms = [["m_g", 10, m1_left, 6], ["m_y", 18, m2_left, 4], ["m_os", 4, m3_left, 2], ["m_s", 4, m4_left, 2]];
+    ms = [["m_g", 6, m1_left, 6], ["m_y", 18, m2_left, 4], ["m_os", 4, m3_left, 2], ["m_s", 4, m4_left, 2]];
     //---------------------------------------------------------------------------------------------
     m_g.innerHTML = array_menu[0];
     m_y.innerHTML = array_menu[1];
@@ -44,7 +46,18 @@ function Sait_load() {
     m1.innerHTML = str;
     for (i = 0; i < ms[0][1] - 1; i++)document.getElementById(ms[0][0] + '_' + (i + 1)).innerText = array_menu_1[i];
     //---------------------------------------------------------------------------------------------
-
+    //--------------------------------------------------------
+    for (j = 0; j < 4; j++) {
+        m = [];
+        for (i = 0; i < ms[j][1]; i++) { m[i] = document.getElementById(ms[j][0] + "_" + i); }
+        m[0].style.height = (ms[j][1] - 1) * text_size + "px";
+        t = 2;
+        for (i = 1; i < m.length; i++) {
+            m[i].style.top = t + "px";
+            t += text_size;
+        }
+    }
+    m_i = 0;
 }
 //=================================================================================================
 function all_control() {
