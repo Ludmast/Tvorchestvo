8a3293deaea870a05c7736a3c4e7fa06c2048c7f
Ludmast <illud263@gmail.com>
2024-11-21 20:58:01 +0300

    Update GlavScript.js

--- GlavScript.js
+++ GlavScript.js
@@ -2,8 +2,9 @@
 function Sait_load() {
     menu_play = false;
     bob.style.backgroundColor = '#306090';
-    //bob.onmousemove = 'all_control()';
+    
 
+    bob.style.overflowX = "hidden";
     t = 50;
     text_size = 35;
 
@@ -45,30 +46,30 @@ function Sait_load() {
     menu_width = 260;
 
     m1_left = 20; m2_left = m1_left+ m_g.clientWidth+10; m3_left = m2_left + m_y.clientWidth+10; m4_left = m3_left + m_os.clientWidth+10; m_b = 1;
-    ms = [["m_g", 6, m1_left, 6], ["m_y", 18, m2_left, 4], ["m_os", 4, m3_left, 2], ["m_s", 4, m4_left, 2]];
+    ms = [["m_g", 7, m1_left, 6], ["m_y", 18, m2_left, 4], ["m_os", 4, m3_left, 2], ["m_s", 4, m4_left, 2]];
     //---------------------------------------------------------------------------------------------
     str = "<div id='m_g_0'class='c_m_o'onmouseover='menu_(1,1,0)'onmouseout='menu_(2,1,0)'onmousemove='menu_(4,1,0)'>"
     for (i = 1; i < ms[0][1]; i++)str += "<div id='m_g_" + i + "'class='c_m'onmouseover='menu_(1," + (i + 1) + ",0)'onmouseout='menu_(2," + (i + 1) + ",0)'onclick='menu_(3," + (i + 1) + ",0)'></div>";
-    str += "<hr id='m_g_l_0'class='c_m_line_0'color='black'size='1'/><hr id='m_g_l_1'class='c_m_line_1'color='white'size='1'/><hr id='m_g_l_2'class='c_m_line_4'color='black'size='1'/><hr id='m_g_l_3'class='c_m_line_5'color='white'size='1'/><hr id='m_g_l_4'class='c_m_line_6'color='black'size='1'/><hr id='m_g_l_5'class='c_m_line_7'color='white'size='1'/></div>";
+    str += "<hr id='m_g_l_0'class='c_m_line_0'color='black'size='1'/><hr id='m_g_l_1'class='c_m_line_1'color='white'size='1'/><hr id='m_g_l_2'class='c_m_line_2'color='black'size='1'/><hr id='m_g_l_3'class='c_m_line_3'color='white'size='1'/></div>";
     m1.innerHTML = str;
     for (i = 0; i < ms[0][1] - 1; i++)document.getElementById(ms[0][0] + '_' + (i + 1)).innerText = array_menu_1[i];
     
     //---------------------------------------------------------------------------------------------
     str = "<div id='m_y_0'class='c_m_o'onmouseover='menu_(1,1,1)'onmouseout='menu_(2,1,1)'onmousemove='menu_(4,1,1)'>"
     for (i = 1; i < ms[1][1]; i++)str += "<div id='m_y_" + i + "'class='c_m'onmouseover='menu_(1," + (i + 1) + ",1)'onmouseout='menu_(2," + (i + 1) + ",1)'onclick='menu_(3," + (i + 1) + ",1)'></div>";
-    str += "<hr id='m_g_l_0'class='c_m_line_0'color='black'size='1'/><hr id='m_g_l_1'class='c_m_line_1'color='white'size='1'/><hr id='m_g_l_2'class='c_m_line_4'color='black'size='1'/><hr id='m_g_l_3'class='c_m_line_5'color='white'size='1'/><hr id='m_g_l_4'class='c_m_line_6'color='black'size='1'/><hr id='m_g_l_5'class='c_m_line_7'color='white'size='1'/></div>";
+    str += "<hr id='m_y_l_0' class='c_m_line_0' color='black' size='1'/><hr id='m_y_l_1' class='c_m_line_1' color='white' size='1'/><hr id='m_y_l_2'class='c_m_line_4' color='black' size='1'/><hr id='m_y_l_3'class='c_m_line_5' color='white' size='1'/></div > ";
     m2.innerHTML = str;
     for (i = 0; i < ms[1][1] - 1; i++)document.getElementById(ms[1][0] + '_' + (i + 1)).innerText = array_menu_2[i];
     //---------------------------------------------------------------------------------------------
     str = "<div id='m_os_0'class='c_m_o'onmouseover='menu_(1,1,2)'onmouseout='menu_(2,1,2)'onmousemove='menu_(4,1,2)'>"
     for (i = 1; i < ms[2][1]; i++)str += "<div id='m_os_" + i + "'class='c_m'onmouseover='menu_(1," + (i + 1) + ",2)'onmouseout='menu_(2," + (i + 1) + ",2)'onclick='menu_(3," + (i + 1) + ",2)'></div>";
-    str += "<hr id='m_g_l_0'class='c_m_line_0'color='black'size='1'/><hr id='m_g_l_1'class='c_m_line_1'color='white'size='1'/><hr id='m_g_l_2'class='c_m_line_4'color='black'size='1'/><hr id='m_g_l_3'class='c_m_line_5'color='white'size='1'/><hr id='m_g_l_4'class='c_m_line_6'color='black'size='1'/><hr id='m_g_l_5'class='c_m_line_7'color='white'size='1'/></div>";
+    str += "<hr id='m_os_l_0' class='c_m_line_0' color='black' size='1'/><hr id='m_os_l_1' class='c_m_line_1' color='white' size='1'/></div > ";
     m3.innerHTML = str;
     for (i = 0; i < ms[2][1] - 1; i++)document.getElementById(ms[2][0] + '_' + (i + 1)).innerText = array_menu_3[i];
     //---------------------------------------------------------------------------------------------
     str = "<div id='m_s_0'class='c_m_o'onmouseover='menu_(1,1,3)'onmouseout='menu_(2,1,3)'onmousemove='menu_(4,1,3)'>"
     for (i = 1; i < ms[3][1]; i++)str += "<div id='m_s_" + i + "'class='c_m'onmouseover='menu_(1," + (i + 1) + ",3)'onmouseout='menu_(2," + (i + 1) + ",3)'onclick='menu_(3," + (i + 1) + ",3)'></div>";
-    str += "<hr id='m_g_l_0'class='c_m_line_0'color='black'size='1'/><hr id='m_g_l_1'class='c_m_line_1'color='white'size='1'/><hr id='m_g_l_2'class='c_m_line_4'color='black'size='1'/><hr id='m_g_l_3'class='c_m_line_5'color='white'size='1'/><hr id='m_g_l_4'class='c_m_line_6'color='black'size='1'/><hr id='m_g_l_5'class='c_m_line_7'color='white'size='1'/></div>";
+    str += "<hr id='m_s_l_0'class='c_m_line_0' color='black' size='1'/><hr id='m_s_l_1'class='c_m_line_1' color='white' size='1'/></div > ";
     m4.innerHTML = str;
     for (i = 0; i < ms[3][1] - 1; i++)document.getElementById(ms[3][0] + '_' + (i + 1)).innerText = array_menu_4[i];
     //---------------------------------------------------------------------------------------------
@@ -86,8 +87,17 @@ function Sait_load() {
     }
     m_i = 0;
     //---------------------------------------------------------------------------------------------
+    bob.onclick = 'all_control()';
 }
-
+//=================================================================================================
+function all_control() {
+    if (event.clientY > 55) {
+        if (m_g_0.style.visibility == 'visible') { if (event.clientY < 44 || event.clientY > (ms[0][1] + 1) * text_size - 30 || event.clientX < ms[0][2] || event.clientX > ms[0][2] + menu_width - 10) menu_vis(); }
+        if (m_y_0.style.visibility == 'visible') { if (event.clientY < 44 || event.clientY > (ms[1][1] + 1) * text_size - 30 || event.clientX < ms[1][2] || event.clientX > ms[1][2] + menu_width - 10) menu_vis(1); }
+        if (m_os_0.style.visibility == 'visible') { if (event.clientY < 44 || event.clientY > (ms[2][1] + 1) * text_size - 30 || event.clientX < ms[2][2] || event.clientX > ms[2][2] + menu_width - 10) menu_vis(2); }
+        if (m_s_0.style.visibility == 'visible') { if (event.clientY < 44 || event.clientY > (ms[3][1] + 1) * text_size - 30 || event.clientX < ms[3][2] || event.clientX > ms[3][2] + menu_width - 10) menu_vis(3); }
+    }
+}
 //=================================================================================================
 function menu_vis(id_el = 0, deistvie = 0) {
     if (deistvie == 0) deistvie = 'hidden'; else deistvie = 'visible';
@@ -104,7 +114,7 @@ function menu_(deistvie, nomer, idnomer) {
     //---------------------------------------------------------------------------------------------
     m = [];
     m[0] = document.getElementById(ms[idnomer][0]);
-    for (i = 1; i < ms[idnomer][1] + 1; i++) m[i] = document.getElementById(ms[idnomer][0] + "_" + (i - 1));
+    for (i = 1; i < ms[idnomer][1] + 1; i++) m[i] = document.getElementById(ms[idnomer][0] + '_' + (i - 1));
     //---------------------------------------------------------------------------------------------
     switch (deistvie) {
         //-----------------------------------------------------------------------------------------
@@ -113,13 +123,11 @@ function menu_(deistvie, nomer, idnomer) {
                 m[0].style.cursor = "pointer";
                 m[0].style.backgroundColor = color_okna;
                 m[0].style.color = color_videl;
-                //if (menu_play == false) {
-                    if (m_g_0.style.visibility == 'visible') menu_vis();
-                    else if (m_y_0.style.visibility == 'visible') menu_vis(1);
-                    else if (m_os_0.style.visibility == 'visible') menu_vis(2);
-                    else if (m_s_0.style.visibility == 'visible') menu_vis(3);
-                    menu_vis(idnomer, 1);
-                //}
+                    if (idnomer!=0 && m_g_0.style.visibility == 'visible') menu_vis();
+                    else if (idnomer != 1 && m_y_0.style.visibility == 'visible') menu_vis(1);
+                    else if (idnomer != 2 && m_os_0.style.visibility == 'visible') menu_vis(2);
+                else if (idnomer != 3 && m_s_0.style.visibility == 'visible') menu_vis(3);
+                menu_vis(idnomer, 1);
 
             }
             else if (nomer == 1) {
@@ -135,8 +143,7 @@ function menu_(deistvie, nomer, idnomer) {
                 m[0].style.color = "black";
                 m[0].style.backgroundColor = color_menu;
 
-                if ( event.clientY < 45)
-                    menu_vis(idnomer);
+                if (event.clientY < 45) menu_vis(idnomer);
             }
             else {
                 m[nomer].style.backgroundColor = color_okna;
@@ -154,10 +161,10 @@ function menu_(deistvie, nomer, idnomer) {
             break;
         case 4://move
         
-            if (m_g_0.style.visibility == 'visible') { if (event.clientY < 44 || event.clientY > (ms[0][1] + 1) * text_size - 30 || event.clientX < ms[0][2] || event.clientX > ms[0][2] + menu_width-10) menu_vis(); }
-            if (m_y_0.style.visibility == 'visible') { if (event.clientY < 44 || event.clientY > (ms[1][1] + 1) * text_size - 30 || event.clientX < ms[1][2] || event.clientX > ms[1][2] + menu_width-10) menu_vis(1); }
-            if (m_os_0.style.visibility == 'visible') { if (event.clientY < 44 || event.clientY > (ms[2][1] + 1) * text_size - 30 || event.clientX < ms[2][2] || event.clientX > ms[2][2] + menu_width-10) menu_vis(2); }
-            if (m_s_0.style.visibility == 'visible') { if (event.clientY < 44 || event.clientY > (ms[3][1] + 1) * text_size -30 || event.clientX < ms[3][2] || event.clientX > ms[3][2] + menu_width-10) menu_vis(3); }
+            if (m_g_0.style.visibility == 'visible') { if (event.clientY > (ms[0][1] + 1) * text_size - 30 || event.clientX < ms[0][2] || event.clientX > ms[0][2] + menu_width-10) menu_vis(); }
+            if (m_y_0.style.visibility == 'visible') { if (event.clientY > (ms[1][1] + 1) * text_size - 30 || event.clientX < ms[1][2] || event.clientX > ms[1][2] + menu_width-10) menu_vis(1); }
+            if (m_os_0.style.visibility == 'visible') { if (event.clientY > (ms[2][1] + 1) * text_size - 30 || event.clientX < ms[2][2] || event.clientX > ms[2][2] + menu_width-10) menu_vis(2); }
+            if (m_s_0.style.visibility == 'visible') { if (event.clientY > (ms[3][1] + 1) * text_size -30 || event.clientX < ms[3][2] || event.clientX > ms[3][2] + menu_width-10) menu_vis(3); }
             //должна написать перемещение по m_g, m_y,m_os,m_s
             break;
     }
