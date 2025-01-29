053cb04fe6c2f20ba94c24e729684d26fc8d4d87
Ludmast <illud263@gmail.com>
2025-01-30 02:23:09 +0300

    Update GlavScript.js

--- GlavScript.js
+++ GlavScript.js
@@ -12,7 +12,7 @@ function Sait_load() {
     bob.style.backgroundColor = '#306090';
     bob.style.overflowX = 'hidden';
     okna_width = 270;
-    
+   bob.innerHTML=window.screen.orientation; 
 //if (screen.width < screen.height){ 
     //if(innerHeight==screen.width)
 //    bob.innerHTML = screen.width+' ? '+screen.height;
